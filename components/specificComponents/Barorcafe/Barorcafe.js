cb548f87724d4dbcb93b97137694ab9ca73ca1aa
karinhag <karinida.haglund@gmail.com>
2025-01-05 21:39:31 +0100

    css and design

--- components/specificComponents/Barorcafe/Barorcafe.js
+++ components/specificComponents/Barorcafe/Barorcafe.js
@@ -5,37 +5,42 @@ import Hero from "../../genericComponents/Hero/Hero";
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
 
-
 export default class Barorcafe extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-	constructor(props) {
-		super(props);
-	}
+  render() {
+    return (
+      <div {...storyblokEditable(this.props.blok)}>
+        <Headermenu blok={this.props.menu.content}></Headermenu>
 
-	render() {
-		return (
-			<div {...storyblokEditable(this.props.blok)}>
-				<Headermenu blok={this.props.menu.content}></Headermenu> 
-
-
-				<main>
-					<Hero blok={this.props.blok} contentTypeTag="barorcafe" /> {/*This code displays the hero section (title and image)*/}
-
-					{/*The following code displays the intro/description*/}
-					<div className={css["barorcafe-page__main-content"]}>
-						<div id="barorcafe-page__short-description" key="barorcafe-page__short-description" className={css["barorcafe-page__short-description"]}>
-							<section className={css["rich-text-section--with-navigator"]}>
-								<h2 className={css["rich-text-section__title"]}>Barorcafe Details</h2>
-								<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.description })}</div>
-							</section>
-						</div>
-					</div>
-					{/*The following code displays the list*/}
-					{this.props.blok.additionalstuff.map((nestedBlok) => (
-							<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-						))}
-				</main>
-			</div>
-		);
-	}
+        <main>
+          <Hero blok={this.props.blok} contentTypeTag="barorcafe" />{" "}
+          {/*This code displays the hero section (title and image)*/}
+          {/*The following code displays the intro/description*/}
+          <div className={css["barorcafe-page__main-content"]}>
+            <div
+              id="barorcafe-page__short-description"
+              key="barorcafe-page__short-description"
+              className={css["barorcafe-page__short-description"]}
+            >
+              <section className={css["rich-text-section--with-navigator"]}>
+                <h2 className={css["rich-text-section__title"]}>
+                  Barorcafe Details
+                </h2>
+                <div className={css["rich-text-section__rich-text"]}>
+                  {RichTextToHTML({ document: this.props.blok.description })}
+                </div>
+              </section>
+            </div>
+          </div>
+          {/*The following code displays the list*/}
+          {this.props.blok.additionalstuff.map((nestedBlok) => (
+            <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+          ))}
+        </main>
+      </div>
+    );
+  }
 }
