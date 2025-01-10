d487e4209287acafa8056d7c7dccfcc4401fe472
Schnowman <olafmaxdi@gmail.com>
2025-01-10 18:22:44 +0100

    changed the tab title

--- functions/services/metaTagService.js
+++ functions/services/metaTagService.js
@@ -63,16 +63,16 @@ function getDefaultTags() {
 		"googlebot": "all",
 		"robots": "all",
 		// OpenGraph
-		"og:site_name": "TopAcademy",
-		"og:title": "TopAcademy",
+		"og:site_name": "SpotIt",
+		"og:title": "SpotIt",
 		"og:description": description,
 		"og:image": imageUrl,
 		"og:type": "website",
 		// Twitter
-		"twitter:site": "@TopAcademy",
+		"twitter:site": "@SpotIt",
 		"twitter:card": "summary_large_image",
 		"twitter:image": imageUrl,
-		"twitter:title": "TopAcademy",
+		"twitter:title": "SpotIt",
 		"twitter:description": description
 	};
 }
